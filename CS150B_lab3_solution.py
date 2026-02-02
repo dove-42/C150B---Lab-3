b39f38f5a9386dc20a795f4e13048de3d2777354
GinoFrappicino <Gino.Conca@colostate.edu>
2026-02-02 00:56:46 -0700

    Added Solutions for Step 2!

--- CS150B_lab3_solution.py
+++ CS150B_lab3_solution.py
@@ -9,7 +9,21 @@ from DungeonGame import play_game, check_hp
 # print("Thats a lot of " + Debri + "!")
 
 #step 2
+#(Prints)
+# 1): Something is missing in the line below!
+print("Hello, Blimey, Harry, didn't you ever wonder where your mum and dad learned it all?")
+# 2): Fix the line below to correct it!
+print("Learn what?")
+# 3): Add the print statement from the instructions below this line!
+print("Yer a Wizard Harry!")
 
+#(Variables)
+# 4): Something is missing in the line below!
+hero = "Wizard"
+# 5): Something is wrong with the line below! Can you fix it?
+enemy = "Ogre"
+# 6): Create a print statemnent below using both variables created above. Follow the format in the instructions for part 6 in step 2!
+print(hero, "vs", enemy + ". Who will win?")
 
 #step 3
 name = input("What is your name? => ")
