99c0f75b1b5c25ee08703f104944bf69920db5d1
dove-42 <196259711+dove-42@users.noreply.github.com>
2026-02-01 20:05:15 -0700

    Create CS150B_lab3_solution.py
    Add sample solutions for steps 3 and 4.

---
+++ CS150B_lab3_solution.py
@@ -0,0 +1,19 @@
+from DungeonGame import play_game, check_hp
+
+#step 1
+#step 2
+
+
+#step 3
+name = input("What is your name? => ")
+num1 = int(input("Please enter a whole number => "))
+num2 = float(input("Please enter a decimal number => "))
+
+lucky_num = num1 / num2
+
+print("Hello", name, "! Your lucky number is", lucky_num, "today.")
+
+
+#step 4
+name = "Algeron the Great"
+play_game(name)
