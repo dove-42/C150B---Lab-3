f397493fc95af090378f59892b9dec626e84d63c
GinoFrappicino <Gino.Conca@colostate.edu>
2026-02-02 00:54:23 -0700

    Added Step 2!

--- CS150B_lab3.py
+++ CS150B_lab3.py
@@ -6,6 +6,20 @@ print("Rubble")
 print("Thats a lot of " + Debri + "!")
 
 #Step 2
+#(Prints)
+# 1): Something is missing in the line below!
+print "Hello, Blimey, Harry, didn't you ever wonder where your mum and dad learned it all?"
+# 2): Something is wrong with the line below! Can you fix it?
+Print("Learn what?')
+# 3): Add the print statement from the instructions in step 2 below this line!
+
+#(Variables)
+# 4): Something is missing in the line below!
+hero  "Wizard"
+# 5): Something is wrong with the line below! Can you fix it?
+enemy! == "Ogre"
+# 6): Create a print statemnent below using both variables created above. Follow the format in the instructions for part 6 in step 2!
+
 #Step 3
 #Step 4
 #play_game(name)
